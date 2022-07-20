f9e1b8c8ebf12e88ba44d3a0948c42b64479a99f
Rohan-Swain <rohanswain2003@gmail.com>
2022-07-20 10:21:48 +0530

    Mobile-Header: DONE

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import styled from "styled-components";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from '@mui/icons-material/Close';
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isMobile: false
+        };
     }
 
     render() {
+        console.log(this.state.isMobile);
         return (
             <HeaderWrapper>
                 <Circle />
@@ -17,61 +22,83 @@ class Header extends React.Component {
                 </Title>
                 <div>
                     <MenuWrapper>
-                        <li><a title='Home' onClick={() => window.location.reload(false)}>Home</a></li>
-                        <li><a href='#' title='About Me'>About Me</a></li>
-                        <li><a href='https://kanjiapi.dev/' target='blank' title='The API'>The API</a></li>
+                        <li>
+                            <a title="Home" onClick={() => window.location.reload(false)}>Home</a>
+                        </li>
+                        <li>
+                            <a href="#" title="About Me">About Me</a>
+                        </li>
+                        <li>
+                            <a href="https://kanjiapi.dev/" target="blank" title="The API">The API</a>
+                        </li>
                     </MenuWrapper>
-                    <MenuIconWrapper />
+                    <MenuIconWrapper onClick={() => this.setState({ isMobile: true})} />
+                    {this.state.isMobile && (
+                        <MobileMenu>
+                            <CloseIconWrap fontSize="large" onClick = {() => {this.setState({isMobile: false})}} />
+                            <ul>
+                                <li>
+                                    <a title="Home" onClick={() => window.location.reload(false)}>Home</a>
+                                </li>
+                                <li>
+                                    <a href="#" title="About Me">About Me</a>
+                                </li>
+                                <li>
+                                    <a href="https://kanjiapi.dev/" target="blank" title="The API">The API</a>
+                                </li>
+                            </ul>
+                        </MobileMenu>
+                    )}
                 </div>
             </HeaderWrapper>
-        )
+        );
     }
 }
 
-export default Header
+export default Header;
 
 const HeaderWrapper = styled.div`
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    height: 70px;
-    filter: blur(1);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  height: 70px;
+  filter: blur(1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Title = styled.div`
-    user-select: none;
-    margin-left: 60px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  user-select: none;
+  margin-left: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    letter-spacing: 9px;
+  }
+
+  span {
+    font-size: 0.8em;
+    font-weight: 600;
+    color: black;
+    letter-spacing: 3px;
+  }
+
+  @media (max-width: 700px) {
+    margin-left: 30px;
 
     h2 {
-        letter-spacing: 9px;
+      letter-spacing: 5px;
     }
 
     span {
-        font-size: 0.8em;
-        font-weight: 600;
-        color: black;
-        letter-spacing: 3px;
+      letter-spacing: 2px;
     }
-
-    @media(max-width: 700px) {
-        margin-left: 30px;
-
-        h2 {
-            letter-spacing: 5px;
-        }
-
-        span {
-            letter-spacing: 2px;
-        }
-    }
-`
+  }
+`;
 
 const MenuWrapper = styled.ul`
     list-style: none;
@@ -97,31 +124,72 @@ const MenuWrapper = styled.ul`
     @media(max-width: 700px) {
         display: none;
     }
-`
+`;
 
 const Circle = styled.div`
-    width: 30em;
-    height: 30em;
-    position: absolute;
-    top: 0;
-    left: 0;
-    border-bottom: 20px double white;
-    border-radius: 0em 0em 30em 0em; 
-    background-color: red;
-    z-index: -1;
+  width: 30em;
+  height: 30em;
+  position: absolute;
+  top: 0;
+  left: 0;
+  border-bottom: 20px double white;
+  border-radius: 0em 0em 30em 0em;
+  background-color: red;
+  z-index: -1;
 
-    @media(max-width: 700px) {
-        width: 15em;
-        height: 15em;
-        border-radius: 0em 0em 15em 0em;
-    }
-`
+  @media (max-width: 700px) {
+    width: 15em;
+    height: 15em;
+    border-radius: 0em 0em 15em 0em;
+  }
+`;
 
 const MenuIconWrapper = styled(MenuIcon)`
-    display: none !important;
-    @media(max-width: 700px) {
-        display: block !important;
-        margin-right: 30px;
-        cursor: pointer;
+  display: none !important;
+  @media (max-width: 700px) {
+    display: block !important;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+`;
+
+const MobileMenu = styled.div`
+  position: fixed;
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(7px);
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+
+  ul {
+    width: 100%;
+    height: 100%;
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    li {
+        font-size: 2em;
+        margin: 0.3em;
+
+        &:hover {
+            color: red;
+        }
+
+        a {
+            text-decoration: none;
+            color: black;
+        }
     }
+  }
+`;
+
+const CloseIconWrap = styled(CloseIcon)`
+  position: absolute;
+  right: 0;
+  margin: 0.5em 0.7em;
 `
